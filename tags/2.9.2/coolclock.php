2001aa9355864250d169043952c7469d7c9d018f
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-04 16:59:31 +0000

    tags!
    git-svn-id: https://plugins.svn.wordpress.org/coolclock@647921 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.9.2/coolclock.php
@@ -0,0 +1,451 @@
+<?php
+/*
+Plugin Name: CoolClock
+Plugin URI: http://status301.net/wordpress-plugins/coolclock/
+Description: Add an analog clock to your sidebar.
+Text Domain: coolclock
+Domain Path: languages
+Version: 2.9.2
+Author: RavanH
+Author URI: http://status301.net/
+*/
+
+/**
+ * CoolClock Class
+ */
+class CoolClock {
+
+	static $plugin_version = '2.9.2';
+
+	static $script_version = '3.0.0-pre';
+
+	static $add_script;
+
+	static $add_moreskins;
+
+	static $add_customskins;
+	
+	static $done_excanvas = false;
+
+	static $defaults = array (
+			'skin' => 'swissRail',
+			'radius' => 100,
+			'noseconds' => false,			// Hide seconds
+			'gmtoffset' => '',			// GMT offset
+			'showdigital' => false,			// Show digital time
+			'scale' => 'linear'			// Define type of clock linear/logarithmic/log reversed
+		);
+
+	static $advanced;
+
+	static $advanced_defaults = array (
+			'subtext' => '',
+			'align' => ''
+		);
+
+	static $default_skins = array (
+	    		'swissRail',
+	    		'chunkySwiss',
+	    		'chunkySwissOnBlack'
+    		);
+
+	static $more_skins = array (
+	    		'fancy',
+	    		'machine',
+	    		'simonbaird_com',
+	    		'classic',
+	    		'modern',
+	    		'simple',
+	    		'securephp',
+	    		'Tes2',
+	    		'Lev',
+	    		'Sand',
+	    		'Sun',
+	    		'Tor',
+	    		'Cold',
+	    		'Babosa',
+	    		'Tumb',
+	    		'Stone',
+	    		'Disc',
+	    		'watermelon'
+	    	);
+
+	static $advanced_skins = array ();
+
+	static $advanced_skins_config = array ();
+
+	static $clock_types = array (
+	    		'linear',
+	    		'logClock',
+	    		'logClockRev'
+	    	);
+
+	// FUNCTIONS //
+
+	function widget( $args, $instance ) {
+
+		$skin = ( isset( $instance['skin'] ) ) 
+			? $instance['skin'] : 'swissRail';
+
+		// add custom skin parameters to the plugin skins array
+		if ( 'custom_'.$this->number == $skin )
+			self::$advanced_skins_config[$skin] = $instance['custom_skin'];
+
+		// set footer script flags
+		self::$add_script = true;
+
+		if ( in_array( $skin, self::$more_skins ) )
+			self::$add_moreskins = true;
+		if ( isset( self::$advanced_skins_config[$skin] ) )
+			self::$add_customskins = true;
+
+		$output = self::canvas( array(
+					'skin' => $skin,
+					'radius' => $instance['radius'],
+					'noseconds' => $instance['noseconds'],
+					'gmtoffset' => $instance['gmtoffset'],
+					'showdigital' => $instance['showdigital'],
+					'scale' => $instance['scale'],
+					'align' => $instance['align'],
+					'subtext' => apply_filters('widget_text', $instance['subtext'], $instance)
+					) );
+		
+		return apply_filters( 'coolclock_widget_advanced', $output, $args, $instance );
+
+	}
+
+	static function update( $new_instance, $instance ) {
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['skin'] = strip_tags( $new_instance['skin'] );
+		$instance['custom_skin'] = strip_tags( $new_instance['custom_skin'] );
+		$instance['radius'] = ( (int) $new_instance['radius'] < 5 ) ? 5 : (int) $new_instance['radius'];
+		$instance['noseconds'] = (bool) $new_instance['noseconds'];
+		$instance['gmtoffset'] = ( $new_instance['gmtoffset'] == '' ) ? '' : (float) $new_instance['gmtoffset'];
+		$instance['showdigital'] = (bool) $new_instance['showdigital'];
+		$instance['scale'] = strip_tags( $new_instance['scale'] );
+		$instance['align'] = strip_tags( $new_instance['align'] );
+
+		if ( current_user_can('unfiltered_html') )
+			$instance['subtext'] =  $new_instance['subtext'];
+		else 
+			// wp_filter_post_kses() expects slashed
+			$instance['subtext'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['subtext']) ) ); 
+		
+
+    		return apply_filters( 'coolclock_widget_update_advanced', $instance, $new_instance );
+
+	}
+
+	static function form( $obj, $instance, $defaults ) {
+
+		$defaults = array ( 
+				'title' => '',
+				'custom_skin' => '',
+			);
+		
+		$defaults = array_merge( $defaults, self::$defaults, self::$advanced_defaults );
+	
+		$instance = wp_parse_args( (array) $instance, $defaults );
+	 
+		$title = esc_attr( $instance['title'] );
+		$subtext = esc_attr( $instance['subtext'] );
+		$custom_skin = esc_attr( $instance['custom_skin'] );
+	
+		// Translatable skin names go here
+		$skin_names = array (
+		    		'swissRail' => __('Swiss Rail','coolclock'),
+		    		'chunkySwiss' => __('Chunky Swiss','coolclock'),
+		    		'chunkySwissOnBlack' => __('Chunky Swiss Black','coolclock'),
+		    		'fancy' => __('Fancy','coolclock'),
+		    		'machine' => __('Machine','coolclock'),
+		    		'simonbaird_com' => __('SimonBaird.com','coolclock'),
+		    		'classic' => __('Classic by Bonstio','coolclock'),
+		    		'modern' => __('Modern by Bonstio','coolclock'),
+		    		'simple' => __('Simple by Bonstio','coolclock'),
+		    		'securephp' => __('SecurePHP','coolclock'),
+		    		'Tes2' => __('Tes2','coolclock'),
+		    		'Lev' => __('Lev','coolclock'),
+		    		'Sand' => __('Sand','coolclock'),
+		    		'Sun' => __('Sun','coolclock'),
+		    		'Tor' => __('Tor','coolclock'),
+		    		'Cold' => __('Cold','coolclock'),
+		    		'Babosa' => __('Babosa','coolclock'),
+		    		'Tumb' => __('Tumb','coolclock'),
+		    		'Stone' => __('Stone','coolclock'),
+		    		'Disc' => __('Disc','coolclock'),
+		    		'watermelon' => __('Watermelon by Yoo Nhe','coolclock'),
+		    		'minimal' => __('Minimal','coolclock')
+		    	);
+		    
+	    	$skins = array_merge( self::$default_skins, self::$more_skins, self::$advanced_skins );
+
+		// Translatable type names go here
+		$type_names = array (
+		    		'linear' => __('Linear','coolclock'),
+		    		'logClock' => __('Logarithmic','coolclock'),
+		    		'logClockRev' => __('Logarithmic reversed','coolclock')
+		    	);
+		
+		// Title
+		$output = '<p><label for="' . $obj->get_field_id('title') . '">' . __('Title:') . '</label> ';
+		$output .= '<input class="widefat" id="' . $obj->get_field_id('title') . '" name="' . $obj->get_field_name('title') . '" type="text" value="' . $title . '" /></p>';
+		
+		// Clock settings
+		$output .= '<p><strong>' . __('Clock', 'coolclock') . '</strong></p>';
+		$output .= '<p><label for="' . $obj->get_field_id('skin') . '">' . __('Skin:', 'coolclock') . '</label> ';
+		$output .= '<select class="select" id="' . $obj->get_field_id('skin') . '" name="' . $obj->get_field_name('skin') . '">';
+		foreach ($skins as $value) {
+			$output .= '<option value="' . $value . '"';
+			$output .= ( $value == $instance['skin'] ) ? ' selected="selected">' : '>';
+			$output .= ( isset($skin_names[$value]) ) ? $skin_names[$value] : $value;
+			$output .= '</option>';
+		} unset($value);
+		$output .= '<option value="custom_' . $obj->number . '"';
+		$output .= ( 'custom_'.$obj->number == $instance['skin'] ) ? ' selected="selected">' : '>';
+		$output .= __('Custom (define below)', 'coolclock') . '</option></select></p>';
+		
+		// Custom skin field
+		$output .= '<p><label for="' . $obj->get_field_id('custom_skin') . '">' . __('Custom skin parameters:', 'coolclock') . '</label> ';
+		$output .= '<textarea class="widefat" id="' . $obj->get_field_id('custom_skin') . '" name="' . $obj->get_field_name('custom_skin') . '">' . $custom_skin . '</textarea></p>';
+		
+		// Radius
+		$output .= '<p><label for="' . $obj->get_field_id('radius') . '">' . __('Radius:', 'coolclock') . '</label> ';
+		$output .= '<input class="small-text" id="' . $obj->get_field_id('radius') . '" name="' . $obj->get_field_name('radius') . '" type="number" min="10" value="' . $instance['radius'] . '" /></p>';
+		
+		// Second hand
+		$output .= '<p><input id="' . $obj->get_field_id('noseconds') . '" name="' . $obj->get_field_name('noseconds') . '" type="checkbox" value=';
+		$output .= ( $instance['noseconds'] ) ? '"true" checked="checked" />' : '"false" />';
+		$output .= ' <label for="' . $obj->get_field_id('noseconds') . '">' .  __('Hide second hand', 'coolclock') . '</label></p>';
+
+		// Show digital
+		$output .= '<p><input id="' . $obj->get_field_id('showdigital') . '" name="' . $obj->get_field_name('showdigital') . '" type="checkbox" value=';
+		$output .= ( $instance['showdigital'] ) ? '"true"  checked="checked" />' : '"false" />';
+		$output .= ' <label for="' . $obj->get_field_id('showdigital') . '">' . __('Show digital time', 'coolclock') . '</label></p>';
+		
+		// Show digital
+		$output .= '<p><label for="' . $obj->get_field_id('gmtoffset') . '">' . __('GMT offset:', 'coolclock') . '</label> ';
+		$output .= '<input class="small-text" id="' . $obj->get_field_id('gmtoffset') . '" name="' . $obj->get_field_name('gmtoffset') . '" type="number" step="0.5" value="' . $instance['gmtoffset'] . '" /> ' . __('(leave blank for local time)', 'coolclock') . '</p>';
+		
+		// Scale
+		$output .= '<p><label for="' . $obj->get_field_id('scale') . '">' . __('Scale:', 'coolclock') . '</label> ';
+		$output .= '<select class="select" id="' . $obj->get_field_id('scale') . '" name="' . $obj->get_field_name('scale') . '">';
+		foreach (self::$clock_types as $value) {
+			$output .= '<option value="' . $value . '"';
+			$output .= ( $value == $instance['scale'] ) ? ' selected="selected">' : '>';
+			$output .= ( isset($type_names[$value]) ) ? $type_names[$value] : $value;
+			$output .= '</option>';
+		} unset($value);
+		$output .= '</select></p>';
+
+		// Align
+		$output .= '<p><label for="' . $obj->get_field_id('align') . '">' . __('Align:', 'coolclock') . '</label> ';
+		$output .= '<select class="select" id="' . $obj->get_field_id('align') . '" name="' . $obj->get_field_name('align') . '">';
+		$output .= '<option value=""';
+		$output .= ( $instance['align'] == '' ) ? ' selected="selected">' : '>';
+		$output .= __('none', 'coolclock') . '</option>';		
+		$output .= '<option value="left"';
+		$output .= ( $instance['align'] == 'left' ) ? ' selected="selected">' : '>';
+		$output .= __('left', 'coolclock') . '</option>';
+		$output .= '<option value="right"';
+		$output .= ( $instance['align'] == 'right' ) ? ' selected="selected">' : '>';
+		$output .= __('right', 'coolclock') . '</option>';
+		$output .= '<option value="center"';
+		$output .= ( $instance['align'] == 'center' ) ? ' selected="selected">' : '>';
+		$output .= __('center', 'coolclock') . '</option>';
+		$output .= '</select></p>';
+
+		// Subtext
+		$output .= '<p><label for="' . $obj->get_field_id('subtext') . '">' . __('Subtext:') . '</label> ';
+		$output .= '<input class="widefat" id="' . $obj->get_field_id('subtext') . '" name="' . $obj->get_field_name('subtext') . '" type="text" value="' . $subtext . '" /> ' . __('(basic HTML allowed)', 'coolclock') . '</p>';
+
+		// Advanced filter
+		if ( class_exists( 'CoolClockAdvanced' ) ) // add an upgrade notice
+			$advanced_form .= '<p><strong>' . __('Background') . '</strong></p><p><strong>' . __('Pease upgrade the CoolClock - Pro extension.', 'coolclock') . '</strong> ' . __('You can download the new version using the remaining download credits and the link that you have received in the confirmation email after your first purchase.', 'coolclock') . ' <a href="http://status301.net/contact-en/">' . __('If you do not have that email anymore, please contact us.', 'coolclock') . '</a></p>' . '<p><strong>' . __('Do NOT resave widget settings before upgrading the pro extension or your advanced settings will be lost!', 'coolclock') . '</strong>';
+		else
+	    		$advanced_form = '<p><strong>' . __('Background') . '</strong></p><p><a href="http://status301.net/wordpress-plugins/coolclock-pro/">' . __('Available in the Pro extension &raquo;', 'coolclock') . '</a></p>';
+		
+		$output .= apply_filters( 'coolclock_widget_form_advanced', $advanced_form, $obj, $instance, $defaults );
+
+		return $output;
+	}
+
+	static function go() {
+
+		add_action('plugins_loaded', create_function( '', "return load_plugin_textdomain( 'coolclock', false, dirname(plugin_basename( __FILE__ )).'/languages' );" ) );
+
+		add_action( 'init', array(__CLASS__, 'init' ) );
+	
+		add_action( 'widgets_init', create_function( '', 'return register_widget("CoolClock_Widget");' ) );
+
+	}
+ 
+	static function init() {	
+
+		add_shortcode( 'coolclock', array( __CLASS__, 'handle_shortcode' ) );
+		// allow shortcode in text widgets
+		add_filter('widget_text', 'do_shortcode', 11);
+
+		wp_register_script( 'coolclock', plugins_url('/js/coolclock.min.js', __FILE__), array('jquery'), self::$script_version, true );
+		wp_register_script( 'coolclock-moreskins', plugins_url('/js/moreskins.min.js', __FILE__), array('coolclock'), self::$script_version, true );
+		// could use http://cdnjs.cloudflare.com/ajax/libs/flot/0.7/excanvas.min.js here...
+		wp_register_script( 'excanvas', plugins_url( '/js/excanvas.compiled.js', __FILE__ ), array(), '3', true );
+
+		add_action( 'wp_footer', array( __CLASS__, 'print_scripts' ) );
+
+	}
+
+	static function handle_shortcode( $atts ) {
+
+		$atts = shortcode_atts( array_merge( self::$defaults, self::$advanced_defaults ), $atts );
+
+		// set footer script flags
+		self::$add_script = true;
+		if ( in_array( $atts['skin'], self::$more_skins ) )
+			self::$add_moreskins = true;
+		if ( isset( self::$advanced_skins_config[$atts['skin']] ) )
+			self::$add_customskins = true;
+
+		// return the clock unless it's a feed
+		if ( !is_feed() ) {
+			$output = self::canvas( $atts );
+			return apply_filters( 'coolclock_shortcode_advanced', $output, $atts );
+		} else {
+			return '';	
+		}
+
+	}
+	
+	static function canvas( $atts ) {
+
+		extract( $atts );
+
+		$output = '<div';
+		
+		// align class ans style
+		$output .= ( $align ) ? ' class="align' . $align . '"' : '';
+		$output .= ' style="width:' . 2 * $radius . 'px;height:auto">';
+		// canvas parameters
+		$output .= '<canvas class="CoolClock:' . $skin . ':' . $radius . ':';
+		$output .= ( $noseconds == 'true' ||  $noseconds == '1' ) ? 'noSeconds:' : ':';
+		$output .= $gmtoffset.':';
+		$output .= ( $showdigital == 'true' || $showdigital == '1' ) ? 'showDigital' : '';
+
+		// set type
+		switch ( $scale ) {
+			case 'linear':
+			default:
+				break;
+			case 'logClock':
+				$output .= ':logClock';
+				break;
+			case 'logClockRev':
+				$output .= ':logClockRev';
+		}
+
+		$output .= '"></canvas>';
+		$output .= ( $subtext ) ? '<div style="width:100%;text-align:center;padding-bottom:10px">' . $subtext . '</div></div>' : '</div>';
+		
+		// before returning, try including excanvas which needs to be there before the first canvas...
+		self::print_excanvas();
+
+		return $output;
+
+	}
+ 
+	static function print_excanvas() {
+		
+		if ( self::$done_excanvas )
+			return;
+
+		echo '<!--[if IE]>';
+		wp_print_scripts( 'excanvas' );
+		echo '<![endif]-->
+';
+		self::$done_excanvas = true;
+
+	}
+
+	static function print_scripts() {
+
+		if ( ! self::$add_script )
+			return;
+ 
+		wp_print_scripts( 'coolclock' );
+
+		if ( self::$add_moreskins )
+			wp_print_scripts('coolclock-moreskins');
+
+		if ( self::$add_customskins ) {
+				echo '<script type="text/javascript">jQuery.extend(CoolClock.config.skins, {
+';
+				// loop through plugin custom skins
+				foreach (self::$advanced_skins_config as $key => $value)
+					echo $key.':{'.$value.'},
+';
+				echo '});</script>
+';
+		}
+
+	}
+
+}
+ 
+CoolClock::go();
+
+/**
+ * CoolClock Widget Class
+ */
+class CoolClock_Widget extends WP_Widget {
+
+	/** constructor -- name this the same as the class above */
+	function CoolClock_Widget() {
+		parent::WP_Widget( 
+				'coolclock-widget', 
+				__('Analog Clock', 'coolclock'), 
+				array( 
+					'classname' => 'coolclock', 
+					'description' => __('Add an analog clock to your sidebar.', 'coolclock') 
+					), 
+				array( 
+					'width' => 300, 
+					'id_base' => 'coolclock-widget' 
+					) 
+				);  	
+	}
+ 
+	/** @see WP_Widget::widget -- do not rename this */
+	function widget( $args, $instance ) {
+
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		// Print output
+		echo $before_widget;
+
+		if ( $title )
+			echo $before_title . $title . $after_title;
+
+		echo CoolClock::widget( $args, $instance );
+
+		echo $after_widget;
+
+	}
+
+	/** @see WP_Widget::update -- do not rename this */
+	function update( $new_instance, $old_instance ) {
+
+		return CoolClock::update( $new_instance, $old_instance );
+
+	}
+
+	/** @see WP_Widget::form -- do not rename this */
+	function form( $instance ) {
+
+		// Print output
+		echo CoolClock::form( $this, $instance, $defaults );
+
+	}
+
+}
+
---
+++ tags/2.9.2/readme.txt
@@ -0,0 +1,211 @@
+=== CoolClock - a Javascript Analog Clock ===
+Contributors: RavanH
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=CoolClock%20for%20WordPress&item_number=1%2e3%2e4%2e9&no_shipping=0&tax=0&charset=UTF%2d8&currency_code=EUR
+Tags: clock, analog clock, coolclock, javascript, jquery, widget, shortcode, logarithmic clock
+Requires at least: 2.9
+Tested up to: 3.5
+Stable tag: 2.9.2
+
+Show an analog clock on your WordPress site sidebar or in post and page content.
+
+== Description ==
+
+This plugin integrates [CoolClock - The Javascript Analog Clock](http://randomibis.com/coolclock/) into your WordPress site. You can add it as a **widget** to your sidebar or insert it into your posts and pages with a **shortcode**. See [the FAQ's](http://wordpress.org/extend/plugins/coolclock/faq/) for available shortcode parameters and how to build the clock into your theme.
+
+= Features =
+
+- Can be added via a widget, shortcode or theme function
+- No flash, meaning compatible with pads and most other javascript enabled mobile devices
+- 21 different skins to choose from or
+- Custom skin option to create your own skin style
+- Linear or logarithmic time scale
+
+See [the FAQ's](http://wordpress.org/extend/plugins/coolclock/faq/) to learn how to configure your own skin settings.
+
+= Pro features =
+
+- Custom background image and/or
+- Background color 
+- Border radius
+- One extra skin for use with background image
+- Advanced positioning options
+
+Pro features come with the [CoolClock - Pro extension](http://status301.net/wordpress-plugins/coolclock-pro/).
+
+= Translators =
+
+- **Dutch** * Author: [R.A. van Hagen](http://status301.net/nl/) (version 2.0)
+- **French** * Author: [R.A. van Hagen](http://phareo.eu) (version 2.0)
+
+Please [contact me](http://status301.net/contact-en/) to submit your translation and get mentioned here :)
+
+
+== Installation ==
+
+= Wordpress =
+
+Quick installation: [Install now](http://coveredwebservices.com/wp-plugin-install/?plugin=coolclock) !
+
+ &hellip; OR &hellip;
+
+Search for "coolclock" and install with that slick **Plugins > Add New** back-end page.
+
+ &hellip; OR &hellip;
+
+Follow these steps:
+
+ 1. Download archive.
+
+ 2. Upload the zip file via the Plugins > Add New > Upload page &hellip; OR &hellip; unpack and upload the complete directory with your favourite FTP client to the /plugins/ folder.
+
+ 3. Activate the plugin on the Plug-ins page.
+
+Now visit your Widgets admin page and add the Analog Clock widget to your sidebar. :)
+
+
+== Frequently Asked Questions ==
+
+= Where do I start? =
+
+There is no options page. Just go to your Appearance > Widgets admin page and find the new Analog Clock widget. Add it to your sidebar and change settings if you want to see another than the default clock.
+
+Other ways to integrate a clock into your site are ahortcodes or a theme function. See instructions below.
+
+= What options does the widget have? =
+
+First of all, you can pick a preset skin. There are 21 skins made by other users and one Minimal skin that only shows the clock arms, that can be useful for placing over a custom background image. Then there are:
+
+- Custom skin parameters - see question below;
+- Radius - changes the clock size;
+- Hide second hand;
+- Show digital time;
+- GMT Offset - use this if you want one or more clocks to show the time for other timezones;
+- Scale - linear is our standard clock scale, the other two show a logarithmic time scale;
+- Align - left, center or right;
+- Subtext - optional text, centered below the clock.
+
+Then there are extra options availabe in the [CoolClock - Pro extension](http://status301.net/wordpress-plugins/coolclock-pro/) which allow for more customisation:
+
+- Background image - define the full URL or path to an image to serve as background;
+- Repeat image;
+- Background position - center, top, right, bottom or left of the wrapping div (define div size below);
+- Width and height - define the size of the wrapping div that holds the background image;
+- Background color - define a color value in hex or rgb(a) format, or a css color name;
+- Border radius - optional rounded corners, higher is rounder;
+- Clock position relative to background - here you can position the clock relative to top and left border of the wrapping div (as defined above) that holds the background image.
+
+
+= How can I create a custom skin? =
+
+Here are your first steps into the wonderous world of CoolClock skins ;)
+
+**Step 1.** Copy the following code to a simple unformatted text document (.txt) on your computer.
+
+`
+outerBorder: { lineWidth: 1, radius:95, color: "black", alpha: 1 },
+smallIndicator: { lineWidth: 2, startAt: 89, endAt: 93, color: "black", alpha: 1 },
+largeIndicator: { lineWidth: 4, startAt: 80, endAt: 93, color: "black", alpha: 1 },
+hourHand: { lineWidth: 8, startAt: -15, endAt: 50, color: "black", alpha: 1 },
+minuteHand: { lineWidth: 7, startAt: -15, endAt: 75, color: "black", alpha: 1 },
+secondHand: { lineWidth: 1, startAt: -20, endAt: 85, color: "red", alpha: 1 },
+secondDecoration: { lineWidth: 1, startAt: 70, radius: 4, fillColor: "red", color: "red", alpha: 1 }
+`
+
+**Step 2.** These parameters are the ones from the swissRail skin. Now go and change some parameter values like lineWidth or start/endAt points. The numbers refer to a percentage of the radius, so startAt: 0, endAt: 50 means a line from the center to 50% of the way to the edge. Alpha means the transparency of the element where alpha: 1 means solid. For example alpha: 0.5 means 50% transparent.
+
+**Step 3.** Go to you the Analog Clock widget, select *Skin: Custom* and copy your modified code (all of it, not just the modified parts!) into the field **Custom skin parameters**. Then save the widget and reload your website front page (or wherever the clock is visible) to see the result of your work.
+
+See the preset skins in [moreskins.js](http://randomibis.com/coolclock/moreskins.js) for more examples. And have fun tweaking!
+
+
+= Can I share this fantastic custom skin I created? =
+
+If you made a nice skin and would like to share it, then send it to the script creator at simon dot baird at gmail dot com or paste the custom parameters into a [Review](http://wordpress.org/support/view/plugin-reviews/coolclock). 
+
+Thanks for sharing! :)
+
+
+= Can I insert a clock in posts or pages? =
+
+Yes, there is a shortcode available. Start with a default clock by pasting `[coolclock]` into a post.
+
+The following parameters are available:
+
+- **skin** -- must be one of these: 'swissRail' (default skin), 'chunkySwiss', 'chunkySwissOnBlack', 'fancy', 'machine', 'simonbaird_com', 'classic', 'modern', 'simple', 'securephp', 'Tes2', 'Lev', 'Sand', 'Sun', 'Tor', 'Cold', 'Babosa', 'Tumb', 'Stone', 'Disc' or 'watermelon'. If the Pro extension is activated, there is also 'minimal' available. Please note that these names are _case sensitive_.
+- **radius** -- a number to define the clock radius. Do not add 'px' or any other measure descriptor.
+- **noseconds** -- set to true (or 1) to hide the second hand
+- **gmtoffset** -- a number to define a timezone relative the Greenwhich Mean Time. Do not set this parameter to default to local time.
+- **showdigital** -- set to true (or 1) to show the time in digital format too
+- **scale** -- must be one of these: 'linear' (default scale), 'logClock' or  'logClockRev'. Linear is our normal clock scale, the other two show a logarithmic time scale
+- **subtext** -- optional text, centered below the clock
+- **align** -- sets floating of the clock: 'left', 'right' or 'center'
+
+Example: `[coolclock skin="chunkySwissOnBlack" radius="140" showdigital=true align="left"]`
+
+Then there are extra options availabe in the [CoolClock - Pro extension](http://status301.net/wordpress-plugins/coolclock-pro/) which allow for more customisation:
+
+- **background_image** - define full URL or path to an image to serve as background
+- **background_height** - give a height in pixels (default: auto = clock plus subtext height)
+- **background_width** - give a width in pixels  (default: clock width)
+- **background_color** - define a CSS color value in hex, rgb(a) format, or color name
+- **background_position** - CSS positioning like 'left top', 'bottom', '10% 70%' or '10px 20px' (default: 'left top')
+- **background_repeat** - repeat, repeat-x, repeat-y, no-repeat (default: no-repeat)
+- **background_border_radius** - optional rounded corners value, higher is rounder
+
+Example: `[coolclock skin="minimal" radius="63" align="left" background_image="http://i35.tinypic.com/990wtx.png"]`
+
+
+= I'm building my own theme. Is there a theme function available? =
+
+Yes, you can use a built-in WordPress function that parses a shortcode. To place the same clock as in the shortcode example above, anywhere in your theme, use this:
+
+`
+<?php echo do_shortcode('[coolclock skin="chunkySwiss" radius="140" showdigital=true align="left"]'); ?>
+`
+
+
+== Known Issues ==
+
+Please report any issues on the Support tab.
+
+
+== Screenshots ==
+
+1. Example analog clock in sidebar. The background logo is added with the [CoolClock - Pro extension](http://status301.net/wordpress-plugins/coolclock-pro/).
+
+2. Widget settings. The background options are availabe in the [CoolClock - Pro extension](http://status301.net/wordpress-plugins/coolclock-pro/).
+
+
+== Upgrade Notice ==
+
+= 2.9.2 =
+Thread between second hand tip and 3 o'clock in IE work-around.
+
+
+== Changelog ==
+
+= 2.9.2 =
+* FIX: Thread between tip of the second hand and 3 o'clock in IE
+* Shortcode filter
+
+= 2.9 =
+* BUGFIX: excanvas included too late
+* CoolClock.js version 3.0.0-pre
+* Allow shortcode in text widget
+* NEW: Subtext option
+* NEW: Widget align option
+
+= 2.0 =
+* NEW: logClock option
+
+= 1.1 =
+* Minified javascript
+
+= 1.0 =
+* Sidebar widget overhaul
+* Class
+* NEW: Shortcode
+
+= 0.1 =
+* First implementation of CoolClock in sidebar widget
+
